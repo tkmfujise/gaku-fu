e60c1e80e542ba149fb98ae19ae9f73b6120dcd1
FUJISE Takumi <106900036+tkmfujise@users.noreply.github.com>
2026-06-19 01:12:24 +0900

    update

--- docs/assets/js/player.js
+++ docs/assets/js/player.js
@@ -1,36 +1,24 @@
 class Player {
-  canvas     = null
-  controller = null
+  editor = null
 
-  render(target, score) {
-    this.canvas = ABCJS.renderAbc(target, score, {
+  render(editorId, canvasId, playerId) {
+    this.editor = new ABCJS.Editor(editorId, { 
+      canvas_id:   canvasId, 
+      warnings_id: "warnings",
+      abcjsParams: {
         responsive: 'resize',
-      })[0]
-  }
-
-
-  setup(target) {
-    this.controller= new ABCJS.synth.SynthController()
-    this.controller.load(`#${target}`, null, {
-      displayRestart:  true,
-      displayPlay:     true,
-      displayProgress: true,
-      displayWarp:     true,
+        add_classes: true,
+      },
+      synth: {
+        el: `#${playerId}`,
+        cursorControl: null,
+        options: {
+          displayRestart:  true,
+          displayPlay:     true,
+          displayProgress: true,
+          displayWarp:     true,
+        }
+      }
     })
-
-    const synth = new ABCJS.synth.CreateSynth()
-    synth.init({ visualObj: this.canvas }).then(() => {
-      this.controller.setTune(this.canvas, false)
-        .then(() => {
-          console.log("Audio loaded successfully.")
-        })
-        .catch((error) => {
-          console.warn("Audio problem: ", error)
-        })
-    })
-  }
-
-  play() {
-    console.log('hello')
   }
 }
