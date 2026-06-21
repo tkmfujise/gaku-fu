a5d20a028073d07e100710cc3abc5cefee378da7
FUJISE Takumi <106900036+tkmfujise@users.noreply.github.com>
2026-06-22 00:57:16 +0900

    update

--- docs/assets/js/player.js
+++ docs/assets/js/player.js
@@ -1,3 +1,60 @@
+class MusicCursorControl {
+  constructor(canvasId) {
+    this.canvasId = canvasId;
+    this.cursor = null;
+  }
+
+  onReady() {}
+  
+  onStart() {
+    const svg = document.querySelector(`#${this.canvasId} svg`);
+    if (!svg) return;
+
+    const oldCursor = svg.querySelector(".abcjs-cursor");
+    if (oldCursor) oldCursor.remove();
+
+    this.cursor = document.createElementNS("http://www.w3.org/2000/svg", "line");
+    this.cursor.setAttribute("class", "abcjs-cursor");
+    this.cursor.setAttribute("style", "stroke: #ff4757; stroke-width: 2px;"); 
+    svg.appendChild(this.cursor);
+  }
+
+  beat(beatNumber) {}
+
+  highlightPlayingNote(ev) {
+    const svg = document.querySelector(`#${this.canvasId} svg`)
+    svg.querySelectorAll('.playing').forEach(e => e.classList.remove('playing'))
+    for (var i = 0; i < ev.elements.length; i++ ) {
+      var note = ev.elements[i];
+      for (var j = 0; j < note.length; j++) {
+        note[j].classList.add("playing");
+      }
+    }
+  }
+
+  onEvent(ev) {
+    if (!this.cursor || !ev.elements || ev.elements.length === 0 || !ev.elements[0][0]) return;
+
+    this.highlightPlayingNote(ev)
+
+    const el = ev.elements[0][0];
+    const bbox = el.getBBox();
+
+    this.cursor.setAttribute("x1", bbox.x - 4);
+    this.cursor.setAttribute("x2", bbox.x - 4);
+    this.cursor.setAttribute("y1", bbox.y - 10);
+    this.cursor.setAttribute("y2", bbox.y + bbox.height + 10);
+  }
+
+  onFinished() {
+    if (this.cursor) {
+      this.cursor.remove();
+      this.cursor = null;
+    }
+  }
+}
+
+
 class Player {
   editor = null
 
@@ -11,7 +68,7 @@ class Player {
       },
       synth: {
         el: `#${playerId}`,
-        cursorControl: null,
+        cursorControl: new MusicCursorControl(canvasId), 
         options: {
           displayRestart:  true,
           displayPlay:     true,
---
+++ lib/filters/index.rb
@@ -0,0 +1,33 @@
+module Nanoc::Filters
+  class Index < Nanoc::Filter
+    identifier :index
+
+    def run(content, params = {})
+      html = content.split.map do |path|
+        dir = dir_for(path)
+        <<~HTML
+          <tr class="audio-list">
+            <td>
+            </td>
+            <td>
+              <a href="#{dir}">#{dir}</a>
+            </td>
+          </tr>
+        HTML
+      end.join
+      <<~HTML
+        <table>
+          <tbody>
+            #{html}
+          </tbody>
+        </table>
+      HTML
+    end
+
+    private
+      def dir_for(path)
+        path.sub(/\.abc$/, '')
+      end
+  end
+end
+
